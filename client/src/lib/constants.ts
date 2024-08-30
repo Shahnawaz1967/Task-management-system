e7a978c609b1200015c456584d87a99c3260d62f
Shahnawaz <ashahnawaz010@gmail.com>
2024-08-30 20:05:28 +0530

    update

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,3 +1,1 @@
-// export const SERVER_URL= import.meta.env.VITE_SERVER_URL;
-
-export const SERVER_URL = "https://task-management-system-hazel-phi.vercel.app/";
+export const SERVER_URL= import.meta.env.VITE_SERVER_URL;
