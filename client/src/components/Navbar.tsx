6df966057b6c4ff2729df411864db70aa362c0a6
Shahnawaz <ashahnawaz010@gmail.com>
2024-08-30 20:32:51 +0530

    up

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,18 +3,20 @@ import { useAuth } from "@/context/AuthContext";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const { logout } = useAuth();
+  const { logout, isAuthenticated } = useAuth();
+
   return (
     <nav className="border-b">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/" className="flex items-center space-x-2">
           <span className="text-xl font-bold">Task Manager</span>
         </Link>
-        <div className="flex items-center space-x-4 sm:space-x-6">
+        {isAuthenticated && <div className="flex items-center space-x-4 sm:space-x-6">
           <Button size="sm" onClick={logout}>
             Logout
           </Button>
-        </div>
+        </div>}
+        
       </div>
     </nav>
   );
