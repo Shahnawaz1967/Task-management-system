f1af386841b724092c50df8f956a1abd0ae088c3
Shahnawaz <ashahnawaz010@gmail.com>
2024-08-30 20:49:40 +0530

    updated

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { EyeIcon, EyeOffIcon } from "lucide-react"
-import { useNavigate } from "react-router-dom"
-import { SERVER_URL } from "@/lib/constants"
 import { useAuth } from "@/context/AuthContext"
+import { SERVER_URL } from "@/lib/constants"
+import { EyeIcon, EyeOffIcon } from "lucide-react"
+import { useState } from "react"
 
 export default function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
   const [errors, setErrors] = useState({ email: "", password: "" })
-  const navigate = useNavigate()
+ 
 
   const {login}= useAuth();
 
